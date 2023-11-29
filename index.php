5fc08981c6b87ceabd1ae3ff973c38aa3c7d6468
IIODERII <oderrisi@gmail.com>
2023-11-29 14:11:12 +0100

    added dinamic table to page

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 
 include __DIR__ . "/db.php";
 
-var_dump($hotels);
-
 ?>
 
 <!DOCTYPE html>
@@ -12,11 +10,24 @@ var_dump($hotels);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>PHP Hotel</title>
 </head>
 
-<body>
+<body class='bg-secondary'>
 
+    <header class='container text-center py-5'>
+        <h1>Nice Hotels</h1>
+    </header>
+
+    <main class='container'>
+        <?php include __DIR__ . "/partials/table.php"; ?>
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
---
+++ partials/table.php
@@ -0,0 +1,41 @@
+<?php
+include __DIR__ . '/../db.php';
+?>
+
+<table class="table table-dark table-striped">
+    <thead>
+        <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+            <th scope="col">Distance to Center</th>
+            <th scope="col">Rating</th>
+            <th scope="col">Parking</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        foreach ($hotels as $hotel) {
+            ?>
+            <tr>
+                <th>
+                    <?php echo $hotel['name'] ?>
+                </th>
+                <td>
+                    <?php echo $hotel['description'] ?>
+                </td>
+                <td>
+                    <?php echo $hotel['distance_to_center'] . ' km' ?>
+                </td>
+                <td>
+                    <?php echo $hotel['vote'] . ' &star;' ?>
+                </td>
+                <td>
+                    <div class='w-100 h-100 d-flex justify-content-start align-items-center'>
+                        <div style="width: 1.5em; height: 1.5em"
+                            class='rounded-circle bg-<?php echo $hotel['parking'] ? 'success' : 'danger' ?>'></div>
+                    </div>
+                </td>
+            </tr>
+        <?php } ?>
+    </tbody>
+</table>
